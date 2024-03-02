3f1536866e4242b63da75b017d201c81af617125
Lupi <guadalupemq@gmail.com>
2024-03-02 01:11:56 -0600

    Verificando si es numero primo hasta la raiz del numero más uno.

--- numeros_primos.py
+++ numeros_primos.py
@@ -5,7 +5,7 @@ import time
 inicio = time.time()
 #Función que devuelve True si es número primo y False si no lo es. También imprime cuando es número primo
 def es_primo(num):
-    for n in range(2, num):
+    for n in range(2, int(num**0.5)+1):
         if num % n == 0:
             #print("No es primo", n, "es divisor")
             return False
@@ -19,6 +19,6 @@ print('Los números primos dentro ese intervalo son : ')
 lista = [i for i in range(int(a[0]),int(a[1])) if es_primo(i)]
 #Se reporta la lista de números primos encontrados en el intervalo
 #print(lista)
-# Calcula el timempo de ejecución del programa
+# Calcula el tiempo de ejecución del programa
 fin= time.time()
 print('Tiempo de procesamiento: ', '{:.2f}'.format(fin-inicio))
